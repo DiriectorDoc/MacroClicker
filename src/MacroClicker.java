6b87bd86406093dbba8d5d4ec289845511a7d5d2
Diriector_Doc <davijust213@yahoo.ca>
2020-11-15 01:14:59 -0500

    Got keylistener working

--- src/MacroClicker.java
+++ src/MacroClicker.java
@@ -2,6 +2,9 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,11 +17,33 @@ public class MacroClicker
 	/*static Audio ping = new Audio("C:\\Users\\User\\AppData\\Roaming\\Macro Clicker\\Ping.wav"),
 			     boohw = new Audio("C:\\Users\\User\\AppData\\Roaming\\Macro Clicker\\Boohw.wav");*/
 	
+	private static volatile boolean keyPressed = false;
+	
 	public static void main (String[] args)
 	{
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher(){
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent ke)
+            {
+                synchronized(MacroClicker.class)
+                {
+                    switch (ke.getID())
+                    {
+                    case KeyEvent.KEY_PRESSED:
+                        keyPressed = ke.getKeyCode() == KeyEvent.VK_C;
+                        break;
+                    case KeyEvent.KEY_RELEASED:
+                        keyPressed = false;
+                        break;
+                    }
+                    return false;
+                }
+            }
+        });
 		build();
+		@SuppressWarnings("unused")
 		setInterval clicker = new setInterval(() -> {
-			System.out.println("Hello World");
+			System.out.println(keyPressed);
 		}, 1000);
 	}
 
